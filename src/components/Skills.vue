df8e9de8176167fb0d07a77809bea0638966145e
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-04-06 09:06:13 +0900

    add skill card

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -6,15 +6,25 @@
       <Card fileName="ruby.png" />
       <Card fileName="rails.png" />
       <Card fileName="rspec.png" />
+      <Card fileName="sidekiq.png" />
       <Card fileName="es6.png" />
       <Card fileName="vue.png" />
+    </div>
+    <div class="skills__collection">
+      <Card fileName="mysql.png" />
+      <Card fileName="git.png" />
+      <Card fileName="github.png" />
+      <Card fileName="docker.png" />
+      <Card fileName="esa.png" />
     </div>
     <h3 class="skills--title">in hoby</h3>
     <div class="skills__collection">
       <Card fileName="ionic.png" />
-      <Card fileName="heroku.png" />
-      <Card fileName="firestore.png" />
       <Card fileName="typescript.png" />
+      <Card fileName="firestore.png" />
+      <Card fileName="postgresql.png" />
+      <Card fileName="heroku.png" />
+      <Card fileName="circleci.png" />
     </div>
   </div>
 </template>
